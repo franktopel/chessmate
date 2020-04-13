b01738f24d8b9e3d9b3a6e2c7a57e03a78d6d7b8
Frank Topel <info@connexo.de>
2020-04-13 19:57:45 +0200

    added height and width attributes alongside attributeChangedCallback, made board show startposition, made pieces draggaböe

--- src/js/components/chess-mate/chess-mate.js
+++ src/js/components/chess-mate/chess-mate.js
@@ -6,10 +6,32 @@ export default class ChessMate extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.board = new ChessBoardElement();
+    this.board.setAttribute('draggable-pieces', '');
+    this.board.start();
     this.shadowRoot.appendChild(this.board);
   }
 
   connectedCallback() {}
+
+  static get observedAttributes() {
+    return ['height', 'width'];
+  }
+
+  attributeChangedCallback(attr, oldVal, newVal) {
+    console.log(attr, oldVal, newVal);
+    switch (attr) {
+      case 'height':
+      case 'width':
+        if (newVal) {
+          this.style.height = `${newVal}px`;
+          this.style.width = `${newVal}px`;
+          this.removeAttribute(attr);
+        }
+        break;
+      default:
+        break;
+    }
+  }
 }
 
 customElements.define('chess-mate', ChessMate);
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import './components/chess-mate/chess-mate';
+
+const chessmate = document.createElement('chess-mate');
+document.body.appendChild(chessmate);
+chessmate.setAttribute('height', '400');
+chessmate.setAttribute('width', '400');
+console.log(chessmate.attributes);
